65079e38f91e1e7b8272212b6a17115cfdb50a9d
João Pedro <106919567+joaozzns@users.noreply.github.com>
2025-08-01 17:49:00 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,12 +9,12 @@ const Footer = () => {
             <p className="leading-relaxed opacity-80">
               Cada pessoa tem seu tempo, seu ritmo e sua história. Aqui, você encontra um espaço para ser ouvido(a), acolhido(a) e guiado(a) com empatia no seu processo de transformação.
             </p>
-
+             <br> 
              <p className="leading-relaxed opacity-80">
               Se você vive com depressão, transtorno bipolar ou ansiedade, saiba que não está só, e que é possível viver com mais equilíbrio e bem-estar.
              </p>
           </div>
-
+             </br>
           <div>
             <h4 className="font-medium mb-4">Links Rápidos</h4>
             <ul className="space-y-2 opacity-80">
